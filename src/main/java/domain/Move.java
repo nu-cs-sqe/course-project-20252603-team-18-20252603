6c474a129c1684fd0586dc6b4a96068c3788e038
ef00100 <125419161+ef00100@users.noreply.github.com>
2026-05-14 14:13:20 -0500

    Refactored Move and MoveTest indentation errors

--- src/main/java/domain/Move.java
+++ src/main/java/domain/Move.java
@@ -2,15 +2,15 @@ package domain;
 
 public class Move {
 
-    private final Piece piece;
-    private final Square from;
-    private final Square to;
-    private Piece capturedPiece = null;
-    private Piece promotionPiece = null;
-    private boolean isEnPassant = false;
-    private boolean isCastle = false;
-    private boolean causedCheck = false;
-    private boolean causedCheckmate = false;
+	private final Piece piece;
+	private final Square from;
+	private final Square to;
+	private Piece capturedPiece = null;
+	private Piece promotionPiece = null;
+	private boolean isEnPassant = false;
+	private boolean isCastle = false;
+	private boolean causedCheck = false;
+	private boolean causedCheckmate = false;
 
 	private Move(Piece piece, Square from, Square to) {
 		this.piece = piece;
@@ -18,46 +18,46 @@ public class Move {
 		this.to = to;
 	}
 
-    public static Move create(Piece piece, Square from, Square to) {
-        if (piece == null) {
-            throw new IllegalArgumentException("Piece cannot be null.");
-        }
-        if (from == null) {
-            throw new IllegalArgumentException("\"from\" Square cannot be null.");
-        }
-        if (to == null) {
-            throw new IllegalArgumentException("\"to\" Square cannot be null.");
-        }
+	public static Move create(Piece piece, Square from, Square to) {
+		if (piece == null) {
+			throw new IllegalArgumentException("Piece cannot be null.");
+		}
+		if (from == null) {
+			throw new IllegalArgumentException("\"from\" Square cannot be null.");
+		}
+		if (to == null) {
+			throw new IllegalArgumentException("\"to\" Square cannot be null.");
+		}
 
-        if (from.getOccupant() != piece) {
-            throw new IllegalArgumentException("Piece must match the occupant of the \"from\" square.");
-        }
-        if (from.getFile() == to.getFile() && from.getRank() == to.getRank()) {
-            throw new IllegalArgumentException("\"from\" and \"to\" squares cannot be the same.");
-        }
+		if (from.getOccupant() != piece) {
+			throw new IllegalArgumentException("Piece must match the occupant of the \"from\" square.");
+		}
+		if (from.getFile() == to.getFile() && from.getRank() == to.getRank()) {
+			throw new IllegalArgumentException("\"from\" and \"to\" squares cannot be the same.");
+		}
 
-        return new Move(piece, from, to);
-    }
+		return new Move(piece, from, to);
+	}
 
-    Piece getPiece() {
-        return piece;
-    }
+	Piece getPiece() {
+		return piece;
+	}
 
-    Square getFrom() {
-        return from;
-    }
+	Square getFrom() {
+		return from;
+	}
 
-    Square getTo() {
-        return to;
-    }
+	Square getTo() {
+		return to;
+	}
 
-    Piece getCapturedPiece() {
-        return capturedPiece;
-    }
+	Piece getCapturedPiece() {
+		return capturedPiece;
+	}
 
-    Piece getPromotionPiece() {
-        return promotionPiece;
-    }
+	Piece getPromotionPiece() {
+		return promotionPiece;
+	}
 
 	void setCapturedPiece(Piece capturedPiece) {
 		this.capturedPiece = capturedPiece;
@@ -67,19 +67,19 @@ public class Move {
 		this.promotionPiece = promotionPiece;
 	}
 
-    boolean isEnPassant() {
-        return isEnPassant;
-    }
+	boolean isEnPassant() {
+		return isEnPassant;
+	}
 
-    boolean isCastle() {
-        return isCastle;
-    }
+	boolean isCastle() {
+		return isCastle;
+	}
 
-    boolean isCausedCheck() {
-        return causedCheck;
-    }
+	boolean isCausedCheck() {
+		return causedCheck;
+	}
 
-    boolean isCausedCheckmate() {
-        return causedCheckmate;
-    }
+	boolean isCausedCheckmate() {
+		return causedCheckmate;
+	}
 }
