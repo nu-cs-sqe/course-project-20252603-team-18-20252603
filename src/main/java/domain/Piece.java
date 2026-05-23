8c6cd6c04379cbc5b239aaa4d93d9542a104a357
ef00100 <125419161+ef00100@users.noreply.github.com>
2026-05-23 17:57:48 -0500

    Added Piece stub

--- src/main/java/domain/Piece.java
+++ src/main/java/domain/Piece.java
@@ -1,5 +1,21 @@
 package domain;
 
-// TODO: This is a temporary dummy class so Square can compile.
 public abstract class Piece {
+	Piece(Color color, PieceType type) {
+	}
+
+	Color getColor() {
+		return null;
+	}
+
+	PieceType getType() {
+		return null;
+	}
+
+	void markMoved() {
+	}
+
+	Square[] getMoves(Board board) {
+		return null;
+	}
 }
