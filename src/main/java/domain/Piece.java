b5608c0e7ab66310d3b6889375a063fba4bf753d
eshanhaq4 <59237231+eshanhaq4@users.noreply.github.com>
2026-05-24 22:04:06 -0500

    constructor_nullType_throwsException passes

--- src/main/java/domain/Piece.java
+++ src/main/java/domain/Piece.java
@@ -9,6 +9,9 @@ public abstract class Piece {
 		if (color == null) {
 			throw new IllegalArgumentException("Color can't be null.");
 		}
+		if (type == null) {
+			throw new IllegalArgumentException("Piece type can't be null.");
+		}
 
 		this.color = color;
 		this.type = type;
