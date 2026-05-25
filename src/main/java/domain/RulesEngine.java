3713a3c511891daa184e5425b532f602e9d6bead
davidyim03 <davidyim03@gmail.com>
2026-05-25 03:27:55 -0500

    RulesEngine TC 1 isLegalMove_validPawnSingleStep_returnsTrue passes

--- src/main/java/domain/RulesEngine.java
+++ src/main/java/domain/RulesEngine.java
@@ -1,4 +1,26 @@
 package domain;
 
 public class RulesEngine {
+
+    public boolean isLegalMove(GameModel model, Move move) {
+        Piece piece = move.getPiece();
+
+        if (piece.getColor() != model.getCurrentTurn()) {
+            return false;
+        }
+
+        if (piece.getType() != PieceType.PAWN) {
+            return false;
+        }
+
+        Square from = move.getFrom();
+        Square to = move.getTo();
+
+        int fileDelta = to.getFile() - from.getFile();
+        int rankDelta = to.getRank() - from.getRank();
+
+        return fileDelta == 0
+                && rankDelta == 1
+                && to.getOccupant() == null;
+    }
 }
