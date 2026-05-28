46999759c2ffe52f6518978f37d63cf793f6dd58
ef00100 <125419161+ef00100@users.noreply.github.com>
2026-05-27 22:39:18 -0500

    RulesEngine TC58b isSquareAttacked_rookBlockedPath_returnsFalse passes

--- src/test/java/domain/RulesEngineTest.java
+++ src/test/java/domain/RulesEngineTest.java
@@ -1615,6 +1615,50 @@ class RulesEngineTest {
 		EasyMock.verify(model, board, blackRook);
 	}
 
+	@Test
+	void isSquareAttacked_rookBlockedPath_returnsFalse() {
+		// TC58b: Black rook on e8, white piece on e4 blocks the path to e1.
+		RulesEngine rulesEngine = new RulesEngine();
+
+		GameModel model = EasyMock.createMock(GameModel.class);
+		Board board = EasyMock.createMock(Board.class);
+		Piece blackRook = EasyMock.createMock(Piece.class);
+		Piece whiteBlocker = EasyMock.createMock(Piece.class);
+
+		Square targetSquare = Square.create('e', 1);
+		Square rookSquare = Square.create('e', 8);
+		Square blockerSquare = Square.create('e', 4);
+		rookSquare.setOccupant(blackRook);
+		blockerSquare.setOccupant(whiteBlocker);
+
+		EasyMock.expect(model.getBoard()).andReturn(board).anyTimes();
+		EasyMock.expect(blackRook.getType()).andReturn(PieceType.ROOK).anyTimes();
+		EasyMock.expect(blackRook.getColor()).andReturn(Color.BLACK).anyTimes();
+		EasyMock.expect(whiteBlocker.getColor()).andReturn(Color.WHITE).anyTimes();
+
+		for (char file = 'a'; file <= 'h'; file++) {
+			for (int rank = 1; rank <= 8; rank++) {
+				if (file == 'e' && rank == 8) {
+					EasyMock.expect(board.getSquare(file, rank)).andReturn(rookSquare).anyTimes();
+				} else if (file == 'e' && rank == 4) {
+					EasyMock.expect(board.getSquare(file, rank)).andReturn(blockerSquare).anyTimes();
+				} else if (file == 'e' && rank == 1) {
+					EasyMock.expect(board.getSquare(file, rank)).andReturn(targetSquare).anyTimes();
+				} else {
+					EasyMock.expect(board.getSquare(file, rank)).andReturn(Square.create(file, rank)).anyTimes();
+				}
+			}
+		}
+
+		EasyMock.replay(model, board, blackRook, whiteBlocker);
+
+		boolean result = rulesEngine.isSquareAttacked(model, targetSquare, Color.BLACK);
+
+		assertFalse(result);
+
+		EasyMock.verify(model, board, blackRook, whiteBlocker);
+	}
+
 	// Methods Under Test: isCheckmate
 	// Methods Under Test: isStalemate
 	// Methods Under Test: isCastlingLegal
