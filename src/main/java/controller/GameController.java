ca66651524ec93093dceeb05504cb227790665eb
davidyim03 <davidyim03@gmail.com>
2026-06-06 20:08:12 -0500

    checkstyle fix2

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -26,7 +26,8 @@ public class GameController {
 	private List<Square> selectedLegalMoves;
 	private boolean gameLocked;
 
-	GameController(GameModel model, BoardView boardView, NotificationView notificationView, PromotionView promotionView, CapturedPiecesView capturedView) {
+	GameController(GameModel model, BoardView boardView, NotificationView notificationView, PromotionView promotionView,
+        CapturedPiecesView capturedView) {
 		this.model = model;
 		this.boardView = boardView;
 		this.notificationView = notificationView;
