ce26e0605924e86cd93ff0a58ac7223739d745d4
davidyim03 <davidyim03@gmail.com>
2026-06-08 00:03:50 -0500

    implemented notificationview

--- src/main/java/view/NotificationView.java
+++ src/main/java/view/NotificationView.java
@@ -1,20 +1,51 @@
 package view;
 
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.awt.Font;
+
 public class NotificationView {
+	private final JLabel label;
+
+	public NotificationView() {
+		label = new JLabel("Chess", SwingConstants.CENTER);
+		label.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 22));
+		label.setBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12));
+		label.setOpaque(true);
+		label.setBackground(new Color(245, 245, 245));
+	}
+
+	public JLabel getComponent() {
+		return label;
+	}
 
 	public void showTurn(String color) {
-		// TODO: implement turn notification logic
+		setMessage(displayColor(color) + " to move", new Color(245, 245, 245));
 	}
 
 	public void showCheck(String color) {
-		// TODO: implement check notification logic
+		setMessage(displayColor(color) + " is in check", new Color(255, 224, 178));
 	}
 
 	public void showCheckmate(String winner) {
-		// TODO: implement checkmate notification logic
+		setMessage("Checkmate - " + displayColor(winner) + " wins", new Color(255, 205, 210));
 	}
 
 	public void showStalemate() {
-		// TODO: implement stalemate notification logic
+		setMessage("Stalemate - draw", new Color(225, 225, 225));
+	}
+
+	private void setMessage(String message, Color background) {
+		label.setText(message);
+		label.setBackground(background);
+	}
+
+	private String displayColor(String color) {
+		if (color == null || color.isEmpty()) {
+			return "";
+		}
+		return color.substring(0, 1).toUpperCase() + color.substring(1).toLowerCase();
 	}
 }
